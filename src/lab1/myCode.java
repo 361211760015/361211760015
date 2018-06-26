8a9397a5678b490e69e60c2a8319cc6471c855d3
361211760015 <rattanawalee25400@gmail.com>
2018-06-26 11:34:08 +0700

    Hell World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,26 @@
+package lab1;
+
+import java.util.logging.XMLFormatter;
+
+public class myCode {
+    public static void main(String[] args)  {
+
+        System.out.println("Hell World.");
+        System.out.println("rattanawalee Sakunnoo");
+        System.out.println("361211760015");
+        System.out.println("MIT211");
+        System.out.println("RMUTSV");
+
+
+
+    }//main
+
+
+
+
+
+
+
+
+}//class
+
